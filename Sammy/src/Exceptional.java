0a8fa392de40816ad5273a0886052a8517c4835a
Sammy <samratbabanrakshit@gmail.com>
2025-01-11 13:28:30 +0530

    3rd commit

--- Sammy/src/Exceptional.java
+++ Sammy/src/Exceptional.java
@@ -1,28 +1,23 @@
-import java.io.*; 
+import java.io.*;
 
 public class Exceptional {   //Exception extends Throwable extends Object
-	
-	public static void main (String[] args) {
-	      int a=5;
-	      int b=0;
-	        try{
-	          System.out.println(a/b);
-	        }
-	        
-	      catch(ArithmeticException | NumberFormatException e){  //Checked or Static Exception occurred during compile time like FileNotFoundException
-	       System.out.println("Hello Second");
-	    	  // e.printStackTrace();
-	      }
-	        
-	        catch(Exception e){  //Checked or Static Exception occurred during compile time like FileNotFoundException
-		 	       System.out.println("Hello First");
-		 	    	  // e.printStackTrace();
-		 	       }
-	        
-	        finally
-	        {
-	        	System.out.println("Here I come");
-	        }
-	    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 0;
+        try {
+            System.out.println(a / b);
+        } catch (ArithmeticException | NumberFormatException e) {
+            //Checked or Static Exception occurred during compile time like FileNotFoundException
+
+            System.out.println("Hello Second");
+            // e.printStackTrace();
+        } catch (Exception e) {  //Checked or Static Exception occurred during compile time like FileNotFoundException
+            System.out.println("Hello First");
+            // e.printStackTrace();
+        } finally {
+            System.out.println("Here I come");
+        }
+    }
 
 }
--- Sammy/src/Listu.java
+++ Sammy/src/Listu.java
@@ -1,34 +1,33 @@
 import java.util.*; //java.util.Map & java.util.Collections are 2 root class of Collection Framework.
 
 public class Listu {
-	
-	public static void main (String s[])
-	{
-		List<String> samp = new LinkedList<>() //Double Brace initialization
-				{
-			{
-				add("Ramesh"); //Map, List, Set are interface, can't be instantiated
-				add("Suresh");
-				add("Brijesh");
-				
-			}		//ArrayList uses Dynamic Array to store object whereas LinkedList uses DoublyLinkedList.
-				};   //ArrayList act as List where as LinkedList acts as List & Queue
-				
-				System.out.println(samp);
-				Iterator<String> ita = samp.iterator();
-				System.out.println(ita.next());
-				
-				System.out.println("Second element - " +samp.get(1));
-				samp.set(2,"Dharmesh");
-				System.out.println(samp);
-				
-				Collections.sort(samp);
-				System.out.println(samp);
-				
-				samp.remove(2);
-				System.out.println(samp);
-				System.out.println(samp.hashCode());
-			
 
-	}
+    public static void main(String s[]) {
+        List<String> samp = new LinkedList<>() //Double Brace initialization
+        {
+            {
+                add("Ramesh"); //Map, List, Set are interface, can't be instantiated
+                add("Suresh");
+                add("Brijesh");
+
+            }        //ArrayList uses Dynamic Array to store object whereas LinkedList uses DoublyLinkedList.
+        };   //ArrayList act as List where as LinkedList acts as List & Queue
+
+        System.out.println(samp);
+        Iterator<String> ita = samp.iterator();
+        System.out.println(ita.next());
+
+        System.out.println("Second element - " + samp.get(1));
+        samp.set(2, "Dharmesh");
+        System.out.println(samp);
+
+        Collections.sort(samp);
+        System.out.println(samp);
+
+        samp.remove(2);
+        System.out.println(samp);
+        System.out.println(samp.hashCode());
+
+
+    }
 }
