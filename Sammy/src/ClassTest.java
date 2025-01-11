bc1a063adb48f34770d8f25477da410efb5e160f
Sammy <samratbabanrakshit@gmail.com>
2025-01-11 12:40:14 +0530

    2nd commit

--- Sammy/src/ClassTest.java
+++ Sammy/src/ClassTest.java
@@ -1,13 +1,21 @@
 
 public class ClassTest {
-		public static void main(String[ ] args)
-		{
-		System.out.println(num()) ;
-		}
+    public static void main(String[] args) {
 
-		
-		static int num()
-		{
-		return 77;
-		}
+        String sent = "bangalore is capital of karnataka";
+        ClassTest m = new ClassTest();
+        String newSent = m.senMethod(sent);
+        System.out.println(newSent);
+    }
+
+    String senMethod(String sent) {
+        String word[] = sent.split(" ");
+        String revSen = "", revWord = "";
+        for (int i = 0; i < word.length; i++) {
+            StringBuilder sb = new StringBuilder(word[i].substring(1));
+			revWord = word[i].substring(0, 1).toUpperCase() + String.valueOf(sb.reverse()) + " ";
+			revSen = revSen + revWord;
+        }
+        return revSen;				//Berolagna Is Clatipa Of Kakatanra
+    }
 }
--- Sammy/src/JSONParsingTest.java
+++ Sammy/src/JSONParsingTest.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.nio.file.*;
-import org.json.JSONObject;
 
 
 public class JSONParsingTest {
@@ -8,9 +7,7 @@ public class JSONParsingTest {
     public static void main(String s[]) throws IOException {
 
 
-        String content = new String(Files.readAllBytes(Paths.get("Sammy/src/sample.json")));
-        JSONParser parser = new JSONParser();
-        JSONObject jo =
+
 
     }
 }
